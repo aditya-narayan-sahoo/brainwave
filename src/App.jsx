24034d7eed4d32607ef780fda89d06d5d3657e74
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-10-24 09:35:50 +0530

    Add resuable button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import ButtionGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
 const App = () => {
-  return <div className="text-3xl underline font-bold">App</div>;
+  return (
+    <>
+      <div className="text-3xl underline font-bold">Brainwave App</div>
+      <div className="pt-[4.75rem] lg:pt-[5.75rem] overflow-hidden">
+        <Button className="mt-10">Something</Button>
+      </div>
+      <ButtionGradient />
+    </>
+  );
 };
 
 export default App;
