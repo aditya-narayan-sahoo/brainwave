692c8ca53282f17761b4b37892ef5534c668beaf
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-10-24 11:42:28 +0530

    Add Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import ButtionGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <div className="text-3xl underline font-bold">Brainwave App</div>
       <div className="pt-[4.75rem] lg:pt-[5.75rem] overflow-hidden">
-        <Button className="mt-10">Something</Button>
+        <Header />
       </div>
       <ButtionGradient />
     </>
