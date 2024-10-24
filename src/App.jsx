185d79da1253a4a1fd279103aa6949b3229adaa2
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-10-24 20:02:44 +0530

    Add benefits component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtionGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.75rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
       <ButtionGradient />
     </>
